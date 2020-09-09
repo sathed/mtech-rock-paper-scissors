a9d32ad4b3199e2513c3adc799f5ef0bc6dba6be
Clint Pedersen <cpedersen@verisk.com>
2020-09-08 22:59:43 -0600

    Initial commit.

---
+++ app.js
@@ -0,0 +1,12 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+exports.handler = async(event, context) => {
+    // Determine the winning choice
+    // Rock loses to paper, paper loses to scissors, etc.
+
+    // Create the object in S3
+    // bucket/yourname/results.txt
+
+    // Return the string
+};
